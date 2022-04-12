e84543110c1d8d23b2ba1049afd73ae6c7c883be
Ted J <tedmjablonowski@gmail.com>
2022-04-12 10:47:23 -0400

    Testing delete functions, code nearly there

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const allTheNotes = require('./db/db.json');
+const completeNotes = require('./db/db.json');
 
 const express = require('express');
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    res.json(allTheNotes.slice(1));
+    res.json(completeNotes.slice(1));
 });
 
 app.get('/', (req, res) => {
@@ -27,27 +27,46 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-function generateNote(body, allTheNotes) {
+function generateNote(body, notesArray) {
     const note = body;
-    if (!Array.isArray(allTheNotes))
-        allTheNotes = [];
+    if (!Array.isArray(notesArray))
+        notesArray = [];
     
-    if (allTheNotes.length === 0)
-        allTheNotes.push(0);
+    if (notesArray.length === 0)
+        notesArray.push(0);
 
-    body.id = allTheNotes[0];
-    allTheNotes[0]++;
+    body.id = notesArray[0];
+    notesArray[0]++;
 
-    allTheNotes.push(note);
+    notesArray.push(note);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify(allTheNotes, null, 2)
+        JSON.stringify(notesArray, null, 2)
     );
     return note;
 }
 
+app.delete("/api/notes/:id", function(req, res){
+        
+    // Obtains id and converts to a string
+    let id = req.params.id.toString();
+
+    // Goes through notesArray searching for matching ID
+    for (i=0; i < completeNotes.length; i++){
+       
+        if (completeNotes[i].id == id){
+            // responds with deleted note
+            res.send(completeNotes[i]);
+
+            // Removes the deleted note
+            completeNotes.splice(i,1);
+            break;
+        }
+    }
+});
+
 app.post('/api/notes', (req, res) => {
-    const note = generateNote(req.body, allTheNotes);
+    const note = generateNote(req.body, completeNotes);
     res.json(note);
 });
 
